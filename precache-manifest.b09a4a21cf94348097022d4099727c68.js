f673eae7084160d65473017e9dc9c344bff7bd0c
leminhdev <71681828+leminhdev@users.noreply.github.com>
2020-10-03 23:12:27 +0700

    Updates

---
+++ precache-manifest.b09a4a21cf94348097022d4099727c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc20ca441f0db0def97d87782cbf875e",
+    "url": "/todo-app-reactjs/index.html"
+  },
+  {
+    "revision": "2346fd3c2b64982c231c",
+    "url": "/todo-app-reactjs/static/css/main.554d4918.chunk.css"
+  },
+  {
+    "revision": "5520a7bbe6e771cfc33f",
+    "url": "/todo-app-reactjs/static/js/2.9cf77d36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app-reactjs/static/js/2.9cf77d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2346fd3c2b64982c231c",
+    "url": "/todo-app-reactjs/static/js/main.dd86629f.chunk.js"
+  },
+  {
+    "revision": "fd5c8ac5731fd95781e8",
+    "url": "/todo-app-reactjs/static/js/runtime-main.0c52a03c.js"
+  }
+]);
